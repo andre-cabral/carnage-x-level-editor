811a2ff5bfefc3aa3c1f36f8eceb4b0b4efd2e0f
andre-cabral <cabral.dev@gmail.com>
2014-06-01 00:38:44 -0300

    atualizacao do ctrl para pintar multiplos quadrados

--- editor.js
+++ editor.js
@@ -1,3 +1,6 @@
+var controlPressionado = false;
+var posicaoAtualMouse = "";
+
 $('#gerar-mapa').click(function(){
     var largura = $('#largura-mapa').val();
     var altura = $('#altura-mapa').val();
@@ -17,17 +20,50 @@ $('.icone-selecao').click(function(){
 });
 
 $("body").on("click", "td", function() {
-    var classeIconeAdicionar = 'icone-' + $('.icone-selecao.selecionado').html();
-    var classeIconeRemover = 'icone-' + $(this).html();
-    $(this).removeClass(classeIconeRemover);
-    $(this).addClass(classeIconeAdicionar);
-    
-    $(this).html($('.icone-selecao.selecionado').html());
+    pintarQuadrado(this);
+});
+
+$("body").on("mouseenter", "td", function() {
+    posicaoAtualMouse = this;
+    if( controlPressionado ){
+        pintarQuadrado(this);
+    }
+});
+
+$("body").on("mouseout", "td", function() {
+    posicaoAtualMouse = "";
 });
 
 $('#gerar-codigo').click(function(){
     gerarCodigo();
 });
+
+$( document ).keydown(function(e) {
+    var code = e.keyCode || e.which;
+    if(code == 17) { 
+        controlPressionado = true;
+        
+        if(posicaoAtualMouse != ""){
+            pintarQuadrado(posicaoAtualMouse);
+        }
+    }
+});
+
+$( document ).keyup(function(e) {
+    var code = e.keyCode || e.which;
+    if(code == 17) { 
+        controlPressionado = false;
+    }
+});
+
+function pintarQuadrado(quadrado){
+    var classeIconeAdicionar = 'icone-' + $('.icone-selecao.selecionado').html();
+    var classeIconeRemover = 'icone-' + $(quadrado).html();
+    $(quadrado).removeClass(classeIconeRemover);
+    $(quadrado).addClass(classeIconeAdicionar);
+
+    $(quadrado).html($('.icone-selecao.selecionado').html());
+}
 
 function gerarMapa(largura,altura){
     var codigoInterno = '';
